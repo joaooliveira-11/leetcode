8a00a71d62e3aea2a76f377da7ded569c44030bd
João Oliveira <up202108737@edu.fe.up.pt>
2026-01-19 00:56:25 +0000

    add code for group anagrams

---
+++ neetcode/blind75/arrays&hashing/group_anagrams/README.md
@@ -0,0 +1,7 @@
+# Notion notes
+
+https://joaooliveira-11-coding-interviews.notion.site/GroupAnagrams-2eabe1d3127280379454c9d294317d69?pvs=74
+
+## LeetCode ID
+
+[49. Group Anagrams](https://leetcode.com/problems/group-anagrams/description/)
---
+++ neetcode/blind75/arrays&hashing/group_anagrams/group_anagrams.py
@@ -0,0 +1,24 @@
+from typing import List
+
+# Recommended Time and Complexity
+# You should aim for a solution with O(m * n) time and O(m) space,
+# where m is the number of strings and n is the length of the longest string.
+
+def groupAnagrams(strs: List[str]) -> List[List[str]]:
+    # key: tuple(array[26]), value: list[anagrams]
+    anagrams = {}
+    for st in strs:
+        count = [0] * 26
+        for c in st:
+            count[ord(c) - ord('a')] +=1
+        key = tuple(count)
+        if key in anagrams:
+            anagrams[key].append(st)
+        else:
+            anagrams[key] = [st]
+
+    return list(anagrams.values())
+
+if __name__ == "__main__":
+    print(groupAnagrams(strs = ["act","pots","tops","cat","stop","hat"]))
+
