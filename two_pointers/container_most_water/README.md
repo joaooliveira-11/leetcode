82aa874d73eb21b4a38d27eeebc95a13f67ff330
João Oliveira <up202108737@edu.fe.up.pt>
2026-03-09 00:35:33 +0000

    solved: container-with-most-water(rep)

--- two_pointers/container_most_water/README.md
+++ two_pointers/container_most_water/README.md
@@ -2,7 +2,7 @@
 
 ## Links
 
-- [LeetCode](https://leetcode.com/problems/container-with-most-water/)
-- [NeetCode](https://neetcode.io/problems/max-water-container)
+- [LeetCode](https://leetcode.com/problems/container-with-most-water/submissions/1942362151/)
+- [NeetCode](https://neetcode.io/problems/max-water-container/history?list=allNC&submissionIndex=1)
 
 **Difficulty:** Medium
--- two_pointers/container_most_water/container_most_water.py
+++ two_pointers/container_most_water/container_most_water.py
@@ -1,19 +1,16 @@
-from typing import List
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        p1,p2 = 0, len(height) - 1
 
-# You should aim for a solution with O(n) time and O(1) space, where n is the size of the input array.
-def maxArea(heights: List[int]) -> int:
-    # naive solution would be to test all pairs with O(n^2)
+        max_area = -1
 
-    p1, p2 = 0, len(heights) - 1
+        while(p2 > p1):
+            curr_area = (p2-p1) * min(height[p1], height[p2])
+            max_area = max(curr_area, max_area)
 
-    max_area = -1
-    while(p1 != p2):
-        curr_area = (p2-p1) * min(heights[p1], heights[p2]) # Area calculation formula
-        max_area = max(max_area, curr_area)
+            if height[p1] > height[p2]:
+                p2 -=1
+            else:
+                p1 += 1
 
-        if heights[p1] > heights[p2]:
-            p2 -= 1
-        else:
-            p1 += 1
-    
-    return max_area
+        return max_area
