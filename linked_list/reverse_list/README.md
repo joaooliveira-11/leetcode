69de2d7b98768eed6a7311e916524ad113bb4147
João Oliveira <up202108737@edu.fe.up.pt>
2026-03-09 17:43:58 +0000

    solved: reverse-a-linked-list (rep)

--- linked_list/reverse_list/README.md
+++ linked_list/reverse_list/README.md
@@ -3,7 +3,7 @@
 
 ## Links
 
-- [LeetCode](https://leetcode.com/problems/reverse-linked-list/submissions/1906141989/)
-- [NeetCode](https://neetcode.io/problems/reverse-a-linked-list/question?list=blind75)
+- [LeetCode](https://leetcode.com/problems/reverse-linked-list/submissions/1943151927/)
+- [NeetCode](https://neetcode.io/problems/reverse-a-linked-list/history?list=allNC&submissionIndex=1)
 
 **Difficulty:** Easy
--- linked_list/reverse_list/reverse_list.py
+++ linked_list/reverse_list/reverse_list.py
@@ -3,18 +3,21 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        prev = None
-        curr = head
+        if not head:
+            return
 
-        while(curr):
+        prev, curr = None, head
+
+        while curr:
             next = curr.next
-            curr.next = prev # reverse
+            curr.next = prev
             prev = curr
             curr = next
-        
+
         return prev
-            
-            
+
+
+
+        
